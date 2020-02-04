7e914418de24340a86addba77fe35ff407127cf8
Biljith <biljithjayan@gmail.com>
2020-02-04 08:55:32 -0700

    [Week 3] Add recitation material
    Add recitation and demo related files.
    
    Signed-off-by: Biljith <biljithjayan@gmail.com>

---
+++ code/week_3/Code/scripts/main.js
@@ -0,0 +1,38 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/js-icon.png') {
+    myImage.setAttribute ('src','images/js-illus.png');
+  } else {
+    myImage.setAttribute ('src','images/js-icon.png');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Javascript is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Javascript is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
